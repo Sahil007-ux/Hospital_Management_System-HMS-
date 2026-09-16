31bbc3658531cd7822f45d16fe58dd86e0a5496c
Sahil007-ux <sahilfayaz88@gmail.com>
2026-09-16 15:27:00 +0430

    varchar spelling wrong

--- migration/_Nurse.sql
+++ migration/_Nurse.sql
@@ -2,8 +2,8 @@ CREATE TABLE Nurse
 (
 	Nurse_ID INT PRIMARY KEY NOT NULL,
 	Employee_ID INT NOT NULL UNIQUE,
-	Qualification NVCHAR(100) NOT NULL,
-	Shift NVCHAR(20) NOT NULL,
+	Qualification NVARCHAR(100) NOT NULL,
+	Shift NVARCHAR(20) NOT NULL,
     CONSTRAINT FK_Nurse_Employee
 	FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
 
