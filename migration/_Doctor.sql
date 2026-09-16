a4fc3d6b1da777167f741b5bc923eaae6cfc16a6
Sahil007-ux <sahilfayaz88@gmail.com>
2026-09-16 15:55:24 +0430

    s

--- migration/_Doctor.sql
+++ migration/_Doctor.sql
@@ -7,8 +7,6 @@ CREATE TABLE Doctor
 	Specialization VARCHAR(100) NOT NULL,
 	Qualification VARCHAR(100) NOT NULL,
 	Department VARCHAR(100) NOT NULL,
-	FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID),
-	FOREIGN KEY (Department_ID) REFERENCES Department(Department_ID)
 
 );
 
