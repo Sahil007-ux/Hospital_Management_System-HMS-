9ce097cf30720729cc5a5b9fa2cb7247502346aa
M-Faisal-Sharifi <faisalsharifi274@gmail.com>
2026-09-16 11:13:00 +0430

    Employee Table Done

---
+++ migration/_Employee.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Employee
+(
+	Employee_ID INT PRIMARY KEY,
+	Name NVARCHAR(100) NOT NULL,
+	Gender CHAR(7),
+	CONSTRAINT chk_Gender CHECK (Gender IN('Male','Female')),
+	Phone VARCHAR(20) NULL,
+	Email VARCHAR(254) null,
+	Salary DECIMAL(12,2) NOT NULL,
+	State NVARCHAR(50) NULL,
+	City NVARCHAR(50) NULL,
+    CONSTRAINT CK_Employee_Salary
+	CHECK (Salary >=0 )
+
+);
