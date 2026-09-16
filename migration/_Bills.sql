17641986be7e169894b6ac4e5a7910afd84fdc12
Sahil007-ux <sahilfayaz88@gmail.com>
2026-09-16 15:54:42 +0430

    s

--- migration/_Bills.sql
+++ migration/_Bills.sql
@@ -6,8 +6,6 @@ CREATE TABLE Bills
 	Ammount DECIMAL(10,2) NOT NULL,
 	Bill_Date DATE NOT NULL,
 	Payment_Satus VARCHAR(20) NOT NULL,
-	FOREIGN KEY (Patient_ID) REFERENCES Patients(PatientsID),
-	FOREIGN KEY (Addmission_ID) REFERENCES Addmission(Addmission_ID)
 
 
 );
