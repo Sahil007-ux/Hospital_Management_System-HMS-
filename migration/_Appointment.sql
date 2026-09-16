8cbcae332e50c75f3321a515ab03e6f884a9f63c
Sahil007-ux <sahilfayaz88@gmail.com>
2026-09-16 10:31:21 +0430

    Appointmnet sql added

---
+++ migration/_Appointment.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Appointment
+(
+	Appointment_ID INT PRIMARY KEY,
+	Patients_ID INT NOT NULL,
+	Doctor_ID INT NOT NULL,
+	Appointment_Date DATE,
+	Appointment_Time TIME,
+	Reason VARCHAR(300),
+	Status VARCHAR(50,
+
+	FOREIGN KEY (Patinet_ID) REFERENCES Patient(Patient_ID),
+	FOREIGN KEY (Doctor_ID) REFERENCES Doctor(Doctor_ID)
+
+);
