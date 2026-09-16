f30726dc63efadaea12e1e89aaa5f408ddb1bbe8
Sahil007-ux <sahilfayaz88@gmail.com>
2026-09-16 08:41:28 +0430

    Update database SQL script

--- migration/V1__create_hospital_tables.sql
+++ migration/V1__create_hospital_tables.sql
@@ -1,8 +1,11 @@
-create table Patients
+
+CREATE DATABASE HopitalDB
+
+CREATE TABLE Patients
 (
-	PatientsID int identity(1,1) primary key,
-	FistName varchar(50) not null,
-	LastName varchar(50) not null,
-	Gender char(7),
-	constraint chk_Gender check (Gender IN('Male','Female'))
+	PatientsID INT IDENTITY(1,1) primary key,
+	FistName VARCHAR(50) not null,
+	LastName VARCHAR(50) not null,
+	Gender CHAR(7),
+	CONSTRAINT chk_Gender CHECK (Gender IN('Male','Female'))
 );
